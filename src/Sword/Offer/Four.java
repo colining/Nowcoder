8ebb05e4cab7c7384ffbe025c166e3e472d662f8
colining <18810782954@163.com>
2017-03-16 22:06:47 +0800

    Initial commit

---
+++ src/Sword/Offer/Four.java
@@ -0,0 +1,19 @@
+package Sword.Offer;
+
+import java.util.Arrays;
+
+/**
+ * Created by asus on 2017/3/16.
+ */
+public class Four {
+    public  static  void  main(String [] arg)
+    {
+        StringBuffer a =  new StringBuffer("hello world ");
+        System.out.println(Four.replaceSpace(a));
+    }
+    public static String replaceSpace(StringBuffer str) {
+
+
+        return " ";
+    }
+}

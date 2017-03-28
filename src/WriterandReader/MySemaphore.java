d9c4420c15af9f252c3535e986d7b8d332a24a46
colining <18810782954@163.com>
2017-03-28 22:55:15 +0800

    1

---
+++ src/WriterandReader/MySemaphore.java
@@ -0,0 +1,27 @@
+package WriterandReader;
+
+/**
+ * Created by asus on 2017/3/16.
+ */
+public  class MySemaphore {
+    int value;                              //信号量内部记录值
+    public MySemaphore(int v){
+        this.value = v;
+    }
+    public synchronized void p(){           //加锁的，实现原子性，p操作等待
+        value = value-1;
+        if(value<0){
+            try {
+                wait();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+    public synchronized void v(){           //v操作唤醒；
+        value = value+1;
+        if(value<=0){
+            this.notifyAll();
+        }
+    }
+}
---
+++ src/WriterandReader1/ReaderWriter.java
@@ -0,0 +1,83 @@
+package WriterandReader1;
+
+
+import WriterandReader.MySemaphore;
+
+/**
+ * Created by asus on 2017/3/16.
+ */
+class Reader implements Runnable{
+   volatile int count=0;                                          //读者数量
+    MySemaphore rmutex;                                 //读信号量
+    MySemaphore wmutex;                                 //写信号量
+
+    public Reader(int c,MySemaphore r,MySemaphore w){   //构造函数
+        this.count = c;
+        this.rmutex = r;
+        this.wmutex = w;
+    }
+    public void run(){
+        while(true){
+            rmutex.p();                                             //读者等待加锁
+            if(count == 0) wmutex.p();                              //如果是首个读者，就抢夺写者锁么不允许写
+            count++;
+            rmutex.v();                                             //读者释放锁
+            System.out.println(count+" begin read data");
+            rmutex.p();                                             //读者加锁
+            count--;                                                //当前读者完成
+            System.out.println(count+" finish read data");
+            if(count == 0) wmutex.v();                              //当前没有读者，写者锁释放，可以写啦
+            rmutex.v();                                             //释放锁
+//            try {
+//                Thread.sleep((long) (Math.random()*1000));
+//            } catch (InterruptedException e) {
+//                e.printStackTrace();
+//            }
+        }
+    }
+}
+
+class Writer implements Runnable{
+    MySemaphore wmutex;
+    public Writer(MySemaphore w){
+        this.wmutex = w;
+    }
+    public void run(){
+        while(true){
+            wmutex.p();
+            System.out.println("begin write data");
+            System.out.println("finish write data");
+            wmutex.v();
+            try {
+                Thread.sleep((long) (Math.random()*1000));
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
+
+public class ReaderWriter{
+    public static void main(String[] args){
+        int count = 0;
+        MySemaphore rmutex = new MySemaphore(1);
+        MySemaphore wmutex = new MySemaphore(1);
+        Reader reader = new Reader(count,rmutex,wmutex);
+        Writer writer = new Writer(wmutex);
+        Thread r = new Thread(reader);
+        Thread r1 = new Thread(reader);
+        Thread w = new Thread(writer);
+        Thread w1 = new Thread(writer);
+        w1.start();
+        r.start();
+        w.start();
+        r1.start();
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+
+    }
+}  

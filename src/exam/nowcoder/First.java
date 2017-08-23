c44166aa7259350c03c3e03165d654b1aba88630
colining <18810782954@163.com>
2017-08-23 22:06:09 +0800

    add nowcoder exam

---
+++ src/exam/nowcoder/First.java
@@ -0,0 +1,42 @@
+package exam.nowcoder;
+
+import java.util.Scanner;
+
+/**
+ * Created by colin on 2017/8/23.
+ */
+public class First {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        while (scanner.hasNext()) {
+            int minSize = scanner.nextInt();
+            int maxSize = scanner.nextInt();
+            int n = scanner.nextInt();
+            int[] fish = new int[n];
+            for (int i = 0; i < n; i++) {
+                fish[i] = scanner.nextInt();
+            }
+            func(fish, minSize, maxSize);
+        }
+    }
+
+    private static void func(int[] fish, int minSize, int maxSize) {
+        int[] map = new int[maxSize + 1];
+        for (int i = 0; i < fish.length; i++) {
+            int k = 0;
+            for (int j = (int) Math.ceil(fish[i] / 10.0); j <= (int) Math.floor(fish[i] / 2.0); j++) {
+                map[j] = 1;
+            }
+            for (int j = fish[i]*2; j <=fish[i]*10 && j<=maxSize; j++) {
+                map[j] = 1;
+            }
+        }
+        int sum = 0;
+        for (int i = minSize; i < maxSize+1 ; i++) {
+            if (map[i] == 0) {
+                sum++;
+            }
+        }
+        System.out.println(sum);
+    }
+}
---
+++ src/exam/nowcoder/Second.java
@@ -0,0 +1,67 @@
+package exam.nowcoder;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Scanner;
+
+/**
+ * Created by colin on 2017/8/23.
+ */
+public class Second {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        String[] word = new String[n];
+        String a = scanner.nextLine();
+        for (int i = 0; i < n; i++) {
+            word[i] = scanner.nextLine();
+        }
+        func(word);
+    }
+
+    private static void func(String[] word) {
+        if (word.length == 1) {
+            System.out.println(1);
+        }
+
+        HashSet<String> hashSet = new HashSet<>();
+        for (int i = 0; i < word.length; i++) {
+            boolean flag = true;
+            if (hashSet.isEmpty()) {
+                hashSet.add(word[i]);
+            }else {
+                for (String s : hashSet) {
+                    if (check(s, word[i])) {
+                        flag = false;
+                        break;
+                    }
+                }
+            }
+            if (flag) {
+                hashSet.add(word[i]);
+            }
+        }
+        System.out.println(hashSet.size());
+    }
+
+    private static boolean check(String s, String s1) {
+        if (s.length() != s1.length()) {
+            return false;
+        }
+        int k = 0;
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(0) == s1.charAt(i)) {
+                if (s.equals(change(s1, i))) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    private static String change(String s1, int i) {
+        String a = s1.substring(i, s1.length()) + s1.substring(0, i);
+        return s1.substring(i, s1.length()) + s1.substring(0, i);
+    }
+
+}
